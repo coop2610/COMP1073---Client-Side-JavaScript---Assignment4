05dc52dee63a6648ee525d129ec45faefbbfb7b0
coop2610 <122917442+coop2610@users.noreply.github.com>
2023-04-13 16:58:48 -0700

    move js to html file

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,19 @@
+/*
+
+	Assignment 2 | COMP1073-02 Client-Side JavaScript
+
+	Group Members: 
+	Abimbola Fasawe - 200472319
+	Amanda Cooper - 200507894 
+
+*/
+
 var location = document.querySelector('location');
 var display = document.querySelector('pre');
 let url;
 
-//const baseURL = 'https://api.weatherstack.com/current';
-//const key = 'b1791ffb12a04e6011cbb08f8186e847';
+const baseURL = 'https://api.weatherstack.com/current';
+const key = 'b1791ffb12a04e6011cbb08f8186e847';
 /*
 function fetchResults(event){
     event.preventDefault();
