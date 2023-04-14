0cf2d8104836a8b9b90ab4645f1385feef1f2454
coop2610 <122917442+coop2610@users.noreply.github.com>
2023-04-13 17:18:50 -0700

    js file

--- js/weather.js
+++ js/weather.js
@@ -8,13 +8,13 @@
 
 */
 
-var location = document.querySelector('location');
+var search = document.querySelector('location');
 var display = document.querySelector('pre');
 let url;
 
 const baseURL = 'https://api.weatherstack.com/current';
 const key = 'b1791ffb12a04e6011cbb08f8186e847';
-/*
+
 function fetchResults(event){
     event.preventDefault();
     url = `${baseURL}?access_key=${key}&query=${location.value}&forecast=1`;
@@ -27,9 +27,7 @@ function fetchResults(event){
 
 function displayResults(json) {
     console.log(json);
-
-//comment2
-
+}
 
     
     while (section.firstChild) {
